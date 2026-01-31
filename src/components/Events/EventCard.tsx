7dd1f82db729137609cde993aacbddfca62ab3cc
yasharsanaei <yasharsanayee@gmail.com>
2026-01-31 13:30:14 +0330

    fix: make events container responsive

--- src/components/Events/EventCard.tsx
+++ src/components/Events/EventCard.tsx
@@ -7,21 +7,19 @@ type EventCardProps = {
 
 const EventCard = ({event}: EventCardProps) => {
     const layoutInfo = useMemo(() => {
-        const left = ((event.column / event.columnCount) * 600) + 10;
-        const width = 600 / event.columnCount;
+        const left = ((event.column / event.columnCount) * 100);
+        const width = 100 / event.columnCount;
         const top = event.start;
         const height = event.end - event.start;
         return {left, width, top, height};
     }, [event])
     return <>
         <div className="event-card"
-             key={event?.start}
-             style={{
-                 left: `${layoutInfo.left}px`,
-                 width: `${layoutInfo.width}px`,
-                 top: `${layoutInfo.top}px`,
-                 height: `${layoutInfo.height}px`
-             }}>
+             data-left={layoutInfo.left}
+             data-width={layoutInfo.width}
+             data-top={layoutInfo.top}
+             data-height={layoutInfo.height}
+             key={event?.start}>
             <h2>{event.title}</h2>
             <p>{event.location}</p>
         </div>
